e072bdd43bd04c82fb8b019538af88a8358e3192
yuridevweb <yuridevweb@gmail.com>
2022-05-06 10:42:07 +0100

    navigation to start game working

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
+import React from 'react';
 import Game from './src/components/Game';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import HomeScreen from './src/screens/HomeScreen';
+import GameScreen from './src/screens/GameScreen';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Game />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+        {/* <Game />; */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
