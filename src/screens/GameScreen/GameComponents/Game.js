78a02cd2e5dbb99cd62d90f9247ef306baf8fc68
Diana Vozanu <you@example.com>
2022-05-10 16:35:04 +0100

    random words added and served on game start

--- src/screens/GameScreen/GameComponents/Game.js
+++ src/screens/GameScreen/GameComponents/Game.js
@@ -29,11 +29,13 @@ const copyArray = (arr) => {
 };
 
 const Game = () => {
-  const word = words[0];
+  const [word, setWord]=useState('world')
+ 
+
   const letters = word.split('');
   const remainingLetters = {};
   const [rows, setRows] = useState(
-    new Array(MAX_GUESSES).fill(new Array(letters.length).fill(''))
+    new Array(MAX_GUESSES).fill(new Array(5).fill(''))
   );
   const [wrongLetters, setWrongLetters] = useState('');
   const [currentRow, setCurrentRow] = useState(0);
@@ -44,6 +46,7 @@ const Game = () => {
   const [startTime, setStartTime] = useState();
   const { user, setUser } = useContext(UserContext);
 
+
   const resetGame = () => {
     setRows(new Array(MAX_GUESSES).fill(new Array(letters.length).fill('')));
     setCurrentColumn(0);
@@ -53,6 +56,11 @@ const Game = () => {
   };
 
   useEffect(() => {
+    if(word==='world'){
+
+      setWord(words.words[Math.floor(Math.random() * 2314)])
+    }
+
     if (gameState === 'timeout') {
       checkGameState();
     }
@@ -73,7 +81,7 @@ const Game = () => {
     };
 
     setWrongLetters(removeDuplicates(wrongLetters).join(''));
-  }, [currentRow, gameState]);
+  }, [currentRow, gameState,word]);
 
   const checkGameState = () => {
     if (gameState === 'timeout') {
@@ -81,6 +89,9 @@ const Game = () => {
       Alert.alert(
         'TIME OUT!!',
         `You died! Shoulda thunk faster! 
+        
+        The word was ${word.toUpperCase()}
+
         Guesses Left: ${MAX_GUESSES - currentRow}
         Lives Left: ${lives} 
         Time Left: ${duration - getGameTime()} 
@@ -105,9 +116,12 @@ const Game = () => {
       Alert.alert(
         'THE HANGMAN GOT YOU!!',
         `Your life force is all gone! Haha! 
+
+        The word was ${word.toUpperCase()}
+        
         Guesses Left: ${MAX_GUESSES - currentRow - 1}
         Lives Left: ${lives} 
-        Time Left: ${duration - getGameTime()} 
+        Time Left: ${duration - getGameTime()}
         
         Thank you for playing! 💀
         
@@ -130,6 +144,9 @@ const Game = () => {
       Alert.alert(
         'WINNAR!!',
         `You live!!! For now...
+        
+        The word was ${word.toUpperCase()}
+
         Guesses Left: ${MAX_GUESSES - currentRow} (${getGuessScore()} points)
         Lives Left: ${lives} (${getLivesScore()} points)
         Time Left: ${duration - getGameTime()} (${getTimerScore()} points)
@@ -152,6 +169,9 @@ const Game = () => {
       Alert.alert(
         'YOU DIED!!',
         `You ran out of guesses! 
+
+        The word was ${word.toUpperCase()}
+
         Guesses Left: ${MAX_GUESSES - currentRow}
         Lives Left: ${lives} 
         Time Left: ${duration - getGameTime()} 
